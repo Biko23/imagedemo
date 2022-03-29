3cf80745719449f8fe845291cdc4f091a852d342
Biko23 <bikojeremy@gmail.com>
2022-03-29 09:24:28 +0300

    Create image demo with required dependencies

---
+++ src/main/java/com/twentythreeapps/imagedemo/ImagedemoApplication.java
@@ -0,0 +1,13 @@
+package com.twentythreeapps.imagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagedemoApplication.class, args);
+	}
+
+}
